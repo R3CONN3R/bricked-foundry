42be3c9e3a25310fcc907629368b22a3c58a88ed
R3CONN3R <besler.alex@gmail.com>
2025-01-12 22:11:29 +0100

    feat: Added elevated rail to foundry

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,5 @@
 table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "molten-stone",})
 table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "casting-stone",})
 table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "casting-rail",})
+data.raw.recipe["rail-support"].category="pressing"
+data.raw.recipe["rail-ramp"].category="pressing"
