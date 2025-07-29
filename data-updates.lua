7da65a42aa77c942e9bd983426494a847da48b01
Johan <92191912+Johan482@users.noreply.github.com>
2025-07-29 22:57:41 +0200

    changed name of casting-rail to casting-rail-liquid-stone to fix mod conflicts with More Casting

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,5 @@
 table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "molten-stone",})
 table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "casting-stone",})
-table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "casting-rail",})
+table.insert(data.raw.technology.foundry.effects, {type = "unlock-recipe",recipe = "casting-rail-liquid-stone",})
 data.raw.recipe["rail-support"].category="pressing"
 data.raw.recipe["rail-ramp"].category="pressing"
